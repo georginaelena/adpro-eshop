d2925b1245a0bef922732528d73d9fb555c8274f
georginaelena <georgina.elena@ui.ac.id>
2024-02-07 11:37:59 +0700

    fixing dependencies

--- src/test/java/id/ac/ui/cs/advprog/eshop/model/ProductTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/ProductTest.java
@@ -4,25 +4,26 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 class ProductTest {
     Product product;
 
     @BeforeEach
-    void setup() {
+    void setUp () {
         this.product = new Product();
-        this.product.setProductId("eb558e9F-1c39-460e-8860-71af6af63bd6");
+        this.product.setProductId("eb558e9f-1c39-460e-8860-71af6af63bd6");
         this.product.setProductName("Sampo Cap Bambang");
         this.product.setProductQuantity(100);
     }
 
     @Test
     void testGetProductId() {
-        assertEquals("eb558e9F-1c39-460e-8860-71af6af63bd6", this.product.getProductId());
+        assertEquals("Sampo Cap Bambang", this.product.getProductName());
     }
 
     @Test
     void testGetProductName() {
-        assertEquals("Sampo Cap Bambang", this.product.getProductName());
+        assertEquals("eb558e9f-1c39-460e-8860-71af6af63bd6", this.product.getProductId());
     }
 
     @Test
